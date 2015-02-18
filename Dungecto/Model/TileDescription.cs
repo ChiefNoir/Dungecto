adc1c27a512d248e1fb1df742b2ded307431bb7e
SharpDiv <AliveDiv@gmail.com>
2015-02-19 01:12:03 +0300

    + Map tile description

---
+++ Dungecto/Model/TileDescription.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Dungecto.Model
+{
+    /// <summary> Map tile description </summary>
+    [Serializable]
+    public class TileDescription
+    {
+        /// <summary> Path markup </summary>
+        /// <remarks>https://msdn.microsoft.com/en-us/library/ms752293%28v=vs.110%29.aspx</remarks>
+        [XmlElement("Geometry")]
+        public string GeometryPath { get; set; }
+
+        /// <summary> Tile height (OY-size) </summary>
+        [XmlAttribute("Height")]
+        public double Height { get; set; }
+
+        /// <summary> Color </summary>
+        [XmlAttribute("Height")]
+        public string HexColor { get; set; }
+
+        [XmlAttribute("Name")]
+        public string Name { get; set; }
+
+        /// <summary> Tile width (OX-size) </summary>
+        [XmlAttribute("Width")]
+        public double Width { get; set; }
+
+    }
+}
