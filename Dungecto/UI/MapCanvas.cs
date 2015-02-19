3f36eb8d7f7582a770df311bdd53102338391866
SharpDiv <AliveDiv@gmail.com>
2015-02-19 03:40:55 +0300

    + Map control + Tile control + Tests for Map control

---
+++ Dungecto/UI/MapCanvas.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Dungecto.UI
+{
+    /// <summary> Map canvas </summary>
+    public class MapCanvas : Canvas
+    {
+        /// <summary> <see cref="SelectedTile"/> DependencyProperty </summary>
+        public static readonly DependencyProperty SelectedTileProperty = DependencyProperty.Register
+            (
+                "SelectedTile",
+                typeof(MapTile),
+                typeof(MapCanvas),
+                new FrameworkPropertyMetadata(null)
+            );
+
+        /// <summary> Get selected map tile </summary>
+        public MapTile SelectedTile
+        {
+            get { return (MapTile)GetValue(SelectedTileProperty); }
+            private set { SetValue(SelectedTileProperty, value); }
+        }
+
+        /// <summary> Add tile to map </summary>
+        /// <param name="tile"> Tile to add </param>
+        public void Add(MapTile tile)
+        {
+            if (tile == null) { return; }
+            if (Children.Contains(tile) ) { return; }
+
+            Children.Add(tile);
+        }
+
+        /// <summary> Remove tile from map </summary>
+        /// <param name="tile"> Tile to remove </param>
+        public void Remove(MapTile tile)
+        {
+            Children.Remove(tile);
+        }
+
+        /// <summary> Remove selected tile </summary>
+        public void RemoveSelected()
+        {
+            Remove(SelectedTile);
+        }
+
+    }
+}
---
+++ Dungecto/UI/MapTile.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Dungecto.UI
+{
+    /// <summary> <seealso cref="Dungecto.UI.MapCanvas"/> tile </summary>
+    public class MapTile : ContentControl
+    {
+        /// <summary> <see cref="IsSelected"/> DependencyProperty </summary>
+        public static readonly DependencyProperty IsSelectedProperty = DependencyProperty.Register
+            (
+                "IsSelected",
+                typeof(bool),
+                typeof(MapTile),
+                new FrameworkPropertyMetadata(false)
+            );
+
+        /// <summary>Is tile selected?</summary>
+        public bool IsSelected
+        {
+            get { return (bool)GetValue(IsSelectedProperty); }
+            set { SetValue(IsSelectedProperty, value); }
+        }
+    }
+}
