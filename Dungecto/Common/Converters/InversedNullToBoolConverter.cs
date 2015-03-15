9b672719e3249eee97ed1a96c1123fac35de5511
SharpDiv <AliveDiv@gmail.com>
2015-03-15 18:46:11 +0300

    Fixed. Whe selected tile is null tile properties panel is hidden.

---
+++ Dungecto/Common/Converters/InversedNullToBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Dungecto.Common.Converters
+{
+    /// <summary> Convert object null state to bool, inversed </summary>
+    class InversedNullToBoolConverter : IValueConverter 
+    {
+        /// <summary> Convert object null state to bool, inversed</summary>
+        /// <param name="value">Anything</param>
+        /// <returns>object is null — false</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        /// <summary> [Not implemented] </summary>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
